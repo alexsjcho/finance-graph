34f13b0c195668db42bee21c2abd7b565100cc5b
Alex Cho <alexsjcho@gmail.com>
2018-11-05 09:07:28 -0800

    almost done, troubleshooting stage

--- script1.py
+++ script1.py
@@ -4,57 +4,62 @@ app=Flask(__name__)
 
 @app.route('/plot/')
 def plot():
-    from pandas_datareader import data
+    #Libraries
     import datetime
     import fix_yahoo_finance as yf
     yf.pdr_override()
-    from bokeh.plotting import figure, show, output_file
+    from pandas_datareader import data
+    from bokeh.plotting import figure,show, output_file
     from bokeh.embed import components
     from bokeh.resources import CDN
 
-    start=datetime.datetime(2015,11,1)
+    #Time variables
+    start=datetime.datetime(2016,3,1)
     end=datetime.datetime(2016,3,10)
+    hours_12=12*60*60*1000
 
-    df=data.get_data_yahoo(tickers="GOOG", start=start, end=end)
+    #Price variables
+    date_increase=df.index[df.Close > df.Open]
+    date_decrease=df.index[df.Close < df.Open]
 
+    #Data pulling and Graph rendering
+    df=data.get_data_yahoo(tickers="APPL",data_source="google",start=start,end=end)
+    p=figure(x_axis_type='datatime', width=1000, height=300, sizing_mode="scale_width")
+    p.title.text= "Candlestick Chart"
+    p.grid.grid_line_alpha=0.3
 
-    def inc_dec(c, o):
+    #Open and Close price logic
+    def in_dec(c, o):
         if c > o:
             value="Increase"
         elif c < o:
             value="Decrease"
-        else:
-            value="Equal"
+        else: value="Equal"
         return value
 
-    df["Status"]=[inc_dec(c,o) for c, o in zip(df.Close,df.Open)]
+    #Dataframe variables
+    df["Status"]=[inc_dec(c,o) for c, o in zip(df.Close, df.Open)]
     df["Middle"]=(df.Open+df.Close)/2
     df["Height"]=abs(df.Close-df.Open)
 
-    p=figure(x_axis_type='datetime', width=1000, height=300)
-    p.title.text="Candlestick Chart"
-    p.grid.grid_line_alpha=0.3
+    #Open, Close price visual rendering logic
+    p.segment(df.index,df.High, df.index, df.Low, color="Black")
 
-    hours_12=12*60*60*1000
+    p.rect(df.index[df.Status=="Increase"],df.Middle[df.Status=="Increase"],[df.Close > df.Open],(df.Open_df.Close)/2,df.Height[df.Status=="Increase"],
+    fill_color="green",line_color="black")
 
-    p.segment(df.index, df.High, df.index, df.Low, color="Black")
+    p.rect(df.index[df.Status=="Decrease"],df.Middle[df.Status=="Decrease"],[df.Close < df.Open],(df.Open_df.Close)/2,df.Height[df.Status=="Decrease"],
+    fill_color="red",line_color="black")
 
-    p.rect(df.index[df.Status=="Increase"],df.Middle[df.Status=="Increase"],
-           hours_12, df.Height[df.Status=="Increase"],fill_color="#CCFFFF",line_color="black")
-
-    p.rect(df.index[df.Status=="Decrease"],df.Middle[df.Status=="Decrease"],
-           hours_12, df.Height[df.Status=="Decrease"],fill_color="#FF3333",line_color="black")
-
-    script1, div1 = components(p)
-    cdn_js=CDN.js_files[0]
-    cdn_css=CDN.css_files[0]
+    #Frontend variables
+    script1, div1, = components(p)
+    cdn_js=CDN.js_files
+    cdn_css=CDN.css_files
     return render_template("plot.html",
     script1=script1,
     div1=div1,
     cdn_css=cdn_css,
     cdn_js=cdn_js )
-
-
 
 @app.route('/')
 def home():
--- stock_analysis.py
+++ stock_analysis.py
@@ -1,7 +1,9 @@
 #Libraries
-from pandas_datareader import data
 import datetime
-from bokeh.plotting figure,show, output_file
+from pandas_datareader import data
+from bokeh.plotting import figure,show, output_file
+from bokeh.embed import components
+from bokeh.resources import CDN
 
 #Time variables
 start=datetime.datetime(2016,3,1)
@@ -14,8 +16,9 @@ date_decrease=df.index[df.Close < df.Open]
 
 #Data pulling and Graph rendering
 df=data.DataReader(name="APPL",data_source="google",start=start,end=end)
-p=figure(x_axis_type='datatime', width=1000, height=300)
+p=figure(x_axis_type='datatime', width=1000, height=300, sizing_mode="scale_width")
 p.title.text= "Candlestick Chart"
+p.grid.grid_line_alpha=0.3
 
 #Open and Close price logic
 def in_dec(c, o):
@@ -26,17 +29,28 @@ def in_dec(c, o):
     else: value="Equal"
     return value
 
-#Dataframe variables 
+#Dataframe variables
 df["Status"]=[inc_dec(c,o) for c, o in zip(df.Close, df.Open)]
 df["Middle"]=(df.Open+df.Close)/2
 df["Height"]=abs(df.Close-df.Open)
 
 #Open, Close price visual rendering logic
+p.segment(df.index,df.High, df.index, df.Low, color="Black")
+
 p.rect(df.index[df.Status=="Increase"],df.Middle[df.Status=="Increase"],[df.Close > df.Open],(df.Open_df.Close)/2,df.Height[df.Status=="Increase"],
 fill_color="green",line_color="black")
 
 p.rect(df.index[df.Status=="Decrease"],df.Middle[df.Status=="Decrease"],[df.Close < df.Open],(df.Open_df.Close)/2,df.Height[df.Status=="Decrease"],
 fill_color="red",line_color="black")
 
-output_file("CS.html")
-show(p)
+#Frontend variables
+script1, div1, = components(p)
+cdn_js=CDN.js_files[0]
+cdn_css=CDN.css_files[0]
+return render_template("plot.html",
+script1=script1, div1=div1,
+cdn_css=cdn_css, cdn_js=cdn_js)
+
+#Call scripts
+#output_file("CS.html")
+#show(p)
